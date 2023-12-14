181a1ee44da4ff651d5b4b9449b748381a15c6b8
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-12-14 04:04:56 -0800

    Feature Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   # 'ADMIN_NAME' and 'ADMIN_PASSWORD' respectively, to authenticate users.
   http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASSWORD"]
-  def show
-  end
+def show # Define a method named 'show'.
+  @products = Product.all.length # Retrieve the total number of products from the 'Product' model and assign it to the '@products' instance variable.
+  @categories = Category.all.length # Retrieve the total number of categories from the 'Category' model and assign it to the '@categories' instance variable.
+end # End of the 'show' method.
 end
